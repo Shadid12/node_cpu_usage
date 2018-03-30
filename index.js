50a35755b3ad76ed18d75250d1f9ffc54cec6238
shadid12 <shadidhaque2012@gmail.com>
2018-03-29 23:40:20 -0400

    added final

--- index.js
+++ index.js
@@ -11,7 +11,23 @@ stats.start(); // initiate CPU and Memory monitoring
  * */
 // stats.getCpuLast(60);
 
-// Get Memory usage in the last 1 min or 60 seconds
-stats.getMemLast(6);
+// Get Memory usage in the last 5 seconds
+stats.getMemLast(5);
 
-// oStats.starMonitor(1000, 'verbose'); 
+// Get Memory usage in the last 1 min or 60 seconds
+stats.getMemLast(60);
+
+// Get Memory usage in the last 5 min or 300 seconds
+stats.getMemLast(300);
+
+// Get Memory usage in the last 15 min or 900 seconds
+stats.getMemLast(900);
+
+// Get CPU usage in the last 10 seconds
+stats.getCpuLast(10);
+
+// Get CPU usage in the last 30 seconds
+stats.getCpuLast(30);
+
+// Get CPU usage in the last 10 min or 600 seconds
+stats.getCpuLast(600);
